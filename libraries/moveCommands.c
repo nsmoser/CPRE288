aa1555079b7963be0949ba58f6ce5a804cc84180
nsmoser <43865671+nsmoser@users.noreply.github.com>
2020-09-04 09:36:33 -0500

    Update moveCommands.c
    added moveBackward and turnCCW functions
    renamed turnClockwise function to turnCW

--- libraries/moveCommands.c
+++ libraries/moveCommands.c
@@ -14,29 +14,51 @@
  */
 #include "open_interface.h"
 
-void moveForward(oi_t *sensor_data, int cm){            //forward movement function
-    double distance=0;                                  //distance variable, must be double
-                                                        //since sensor distance is a double
-    oi_setWheels(200,200);                              //move forward at moderate speed
-    while(distance<(cm*10)){                            //while measured distance is less than
-                                                        //distance argument
-        oi_update(sensor_data);                         //update sensor data struct
-        distance+=sensor_data->distance;                //add change in distance to
-    }                                                   //measured distance
-    oi_setWheels(0,0);                                  //turn off wheels
+void moveForward(oi_t *sensor, int cm){            //forward movement function
+    double distance=0;                             //distance variable, must be double
+                                                   //since sensor distance is a double
+    oi_setWheels(200,200);                         //move forward at moderate speed
+    while(distance<(cm*10)){                       //while measured distance is less than
+                                                   //distance argument
+        oi_update(sensor);                         //update sensor data struct
+        distance+=sensor->distance;                //add change in distance to
+    }                                              //measured distance
+    oi_setWheels(0,0);                             //turn off wheels
 }
 
-void turnClockwise(oi_t *sensor_data, int degrees){     //clockwise turn function
-    double angle=0;                                     //angle variable, must be double
-                                                        //since sensor angle is double
-    oi_setWheels(200,-200);                             //move left forward and right backward]
-                                                        //turns slow to avoid turning too far
-    while(angle<(degrees-5)){                           //while measured angle is less than
-                                                        //angle argument
-        oi_update(sensor_data);                         //update sensor data struct
-        angle+=sensor_data->angle;                      //add change in angle to
-    }                                                   //measured angle
-    oi_setWheels(0,0);                                  //turn off wheels
+void moveBackward(oi_t *sensor, int cm){           //backwards movement function
+    double distance=0;                             //distance variable, must be double
+    oi_setWheels(-200,-200);                       //move backwards at moderate speed
+    while(distance<(cm*10)){                       //while measured distance is less than
+                                                   //distance argument
+        oi_update(sensor);                         //update sensor struct
+        distance-=sensor->distance;                //"add" change in distance to
+    }                                              //measured distance. Subtracts bc
+                                                   //change in distance is negative
+    oi_setWheels(0,0);                             //turn off wheels
 }
 
+void turnCW(oi_t *sensor, int degrees){            //clockwise turn function
+    double angle=0;                                //angle variable, must be double
+                                                   //since sensor angle is double
+    oi_setWheels(200,-200);                        //move left forward and right backward
+                                                   //turns slow to avoid turning too far
+    while(angle<(degrees-5)){                      //while measured angle is less than
+                                                   //angle argument
+        oi_update(sensor);                         //update sensor data struct
+        angle+=sensor->angle;                      //add change in angle to
+    }                                              //measured angle
+    oi_setWheels(0,0);                             //turn off wheels
+}
 
+void turnCCW(oi_t *sensor, int degrees){           //counter-clockwise turn function
+    double angle=0;                                //angle variable, must be double
+    oi_setWheels(-200,200);                        //move right forward and left backwards
+    while(angle<(degrees-5)){                      //while measured angle is less than
+                                                   //angle argument
+        oi_update(sensor);                         //update sensor data struct
+        angle-=sensor->angle;                      //"add" change in angle to measured
+    }                                              //angle. Subtracts bc change in angle
+                                                   //is negative in this situation
+    oi_setWheels(0,0);                             //turn off wheels
+}
